b93c02be03cef1bec43f4a4073d7b82ae3f0c742
nejestoky <116104609+nejestoky@users.noreply.github.com>
2022-10-18 19:55:30 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+//alert("кириешки");
+
+//var data = confirm("sir");
+//console.log(data)
+//if(data) {
+//  alert("pou");
+//}
+
+//var result = prompt("skika", 20);
+//document.write(result);
+//console.log(result);
+
+
+
+ var ya = null;
+if(confirm("sure?")) {
+  ya = prompt("ur name?");
+  alert("a nu ok bb, " + ya);
+
+ document.write(ya)
+} else {
+  alert("ti negr");
+  document.write("pedrilla")
+}
